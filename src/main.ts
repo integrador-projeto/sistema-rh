f7bd29ac8e25685c3b0e4d01cb627a218e225fa1
AlessonCarlos <alesson.carlos5@gmail.com>
2026-01-16 10:28:45 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  process.env.TZ = '-03:00'
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  app.enableCors()
+  //localhost:5000
+  await app.listen(process.env.PORT ?? 5000);
+}
+bootstrap();
